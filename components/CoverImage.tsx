b383d3cde566b992351d021e74e2270011d3fc59
msgimanila <msgimanila@yahoo.com>
2023-04-27 15:21:02 +0800

    Update CoverImage.tsx
    2

--- components/CoverImage.tsx
+++ components/CoverImage.tsx
@@ -28,7 +28,7 @@ export default function CoverImage(props: CoverImageProps) {
     <div className="sm:mx-0">
       {slug ? (
         <Link href={`/posts/${slug}`} aria-label={title}>
-          {image}
+          
         </Link>
       ) : (
         image
