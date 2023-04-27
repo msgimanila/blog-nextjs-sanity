b8723b7307b082c8a3ad89c69d6de5085f8fbaac
msgimanila <msgimanila@yahoo.com>
2023-04-27 15:14:41 +0800

    Update BlogHeader.tsx
    2

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -18,7 +18,7 @@ export default function BlogHeader({
         <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
           <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
             {title}
-          </h1>
+          </h1><p>test</p>
           <h4
             className={`mt-5 text-center text-lg md:pl-8 md:text-left ${styles.portableText}`}
           >
