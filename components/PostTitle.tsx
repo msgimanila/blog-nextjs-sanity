3a82414204cdd0bf8f673f0fd8f217adbb01ced1
Mario Garcia <msgimanila@yahoo.com>
2024-12-23 10:13:35 +0800

    Update PostTitle.tsx

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -2,6 +2,6 @@ export default function PostTitle({ children }) {
   return (
     <h1 className="mb-12 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl">
       {children}
-    </h1><test1
+    </h1> 
   )
 }
