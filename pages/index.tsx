66b1df98ef53cb63fec6cc37ef2bc9b8878df81e
Mario Garcia <msgimanila@yahoo.com>
2024-12-23 09:26:04 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,31 @@ import { getAllPosts, getSettings } from 'lib/sanity.client'
 import { Post, Settings } from 'lib/sanity.queries'
 import { GetStaticProps } from 'next'
 import { lazy } from 'react'
+import Link from 'next/link'
+
+export default function IndexPage({ posts, settings, loading }) {
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{settings.siteTitle}</h1>
+      <div className="blog-previews">
+        {posts.map((post) => (
+          <article key={post._id}>
+            <Link href={`/post/${post.slug.current}`}>
+              <a>
+                <img src={post.mainImage.url} alt={post.title} />
+                <h2>{post.title}</h2>
+              </a>
+            </Link>
+            <p>{post.excerpt}</p>
+            <time>{new Date(post.publishedAt).toLocaleDateString()}</time>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 const PreviewIndexPage = lazy(() => import('components/PreviewIndexPage'))
 
